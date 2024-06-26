9c2bb249e5d68940bf39b69e3cda0e082c1ca125
Javier Diaz <coderdiaz@gmail.com>
2024-06-25 20:16:13 -0600

    feat: improved links

---
+++ src/lib/constants.ts
@@ -0,0 +1,11 @@
+export const social = {
+  x: 'https://x.com/coderdiaz',
+  linkedin: 'https://linkedin.com/in/coderdiaz',
+  github: 'https://github.com/coderdiaz',
+  cv: 'https://read.cv/coderdiaz',
+  dribbble: 'https://dribbble.com/coderdiaz',
+  youtube: 'https://youtube.com/@coderdiaz',
+  email: 'mailto:hey@coderdiaz.com',
+}
+
+export const SUB_PAYMENT = 'https://buy.stripe.com/cN2cNt0wYgn4gmI6ou';
