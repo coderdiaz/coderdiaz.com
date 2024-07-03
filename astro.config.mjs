8be599eb41cf0fa41be7aa73cc29a8e3d4169006
Javier Diaz <coderdiaz@gmail.com>
2024-07-02 20:53:09 -0600

    chore: added support for sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,7 +13,11 @@ export default defineConfig({
       },
     },
   },
-  integrations: [react(), tailwind(), mdx({
+  redirects: {
+    '/web-expanish/': '/expanish-website/',
+    '/checkout-expanish/': '/expanish-checkout/',
+  },
+  integrations: [react(), tailwind(), sitemap(), mdx({
     optimize: true,
     syntaxHighlight: 'shiki',
   })]
