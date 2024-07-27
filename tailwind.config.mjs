391a94865740b69b0d86021c9080de7276ccbfb8
Javier Diaz <coderdiaz@gmail.com>
2024-07-26 19:50:41 -0600

    chore: removed unused animatio

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -48,34 +48,8 @@ export default {
       serif: ['Caveat Variable', 'ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
     },
     extend: {
-      animation: {
-        glitch: 'glitch-anim-1 3s infinite',
-      },
-      keyframes: {
-        'glitch-anim-1': {
-          '0%': { clip: 'rect(13px,140px,15px,0)' },
-          '5%': { clip: 'rect(5px,140px,16px,0)' },
-          '10%': { clip: 'rect(16px,140px,6px,0)' },
-          '15%': { clip: 'rect(19px,140px,13px,0)' },
-          '20%': { clip: 'rect(6px,140px,18px,0)' },
-          '25%': { clip: 'rect(12px,140px,5px,0)' },
-          '30%': { clip: 'rect(4px,140px,20px,0)' },
-          '35%': { clip: 'rect(6px,140px,8px,0)' },
-          '40%': { clip: 'rect(17px,140px,7px,0)' },
-          '45%': { clip: 'rect(19px,140px,3px,0)' },
-          '50%': { clip: 'rect(6px,140px,5px,0)' },
-          '55%': { clip: 'rect(3px,140px,13px,0)' },
-          '60%': { clip: 'rect(15px,140px,14px,0)' },
-          '65%': { clip: 'rect(14px,140px,14px,0)' },
-          '70%': { clip: 'rect(8px,140px,15px,0)' },
-          '75%': { clip: 'rect(14px,140px,14px,0)' },
-          '80%': { clip: 'rect(12px,140px,19px,0)' },
-          '85%': { clip: 'rect(2px,140px,14px,0)' },
-          '90%': { clip: 'rect(12px,140px,5px,0)' },
-          '95%': { clip: 'rect(7px,140px,1px,0)' },
-          to: { clip: 'rect(19px,140px,2px,0)' },
-        },
-      },
+      animation: {},
+      keyframes: {},
       typography: (theme) => ({
         DEFAULT: {
           css: {
